90fb96adda7561e3fb6cb1124a480701d0bf4a0a
longfei1992 <longfeiah@hotmail.com>
2017-06-14 07:24:39 +0800

    添加BatchSetTexture

---
+++ Assets/Editor/BatchSetTexture.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Collections.Generic;
+using UnityEditor;
+public class BatchSetTexture
+{
+    [MenuItem("TextureTool/批量设置纹理")]
+    static void BatchSet()
+    {
+        SetTextures();
+    }
+
+    static void SetTextures()
+    {
+        string[] assets = AssetDatabase.FindAssets("t:Texture");
+
+        AssetDatabase.StartAssetEditing();
+
+        foreach (string guid in assets)
+        {
+            string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+            TextureImporter importer = AssetImporter.GetAtPath(assetPath) as TextureImporter;
+            if (importer == null)
+                continue;
+            if (importer.textureType == TextureImporterType.Advanced)
+            {
+                if (importer.textureFormat == TextureImporterFormat.ETC2_RGB4)
+                {
+                    importer.textureFormat = TextureImporterFormat.ASTC_RGB_8x8;
+                    AssetDatabase.ImportAsset(assetPath);
+                }
+                else if (importer.textureFormat == TextureImporterFormat.ETC2_RGBA8)
+                {
+                    importer.textureFormat = TextureImporterFormat.ASTC_RGBA_8x8;
+                    AssetDatabase.ImportAsset(assetPath);
+                }
+                else
+                {
+                    UnityEngine.Debug.Log(assetPath);
+                }
+            }
+        }
+
+        AssetDatabase.StopAssetEditing();
+
+        EditorUtility.DisplayDialog("设置完成", "设置完成", "确定");
+    }
+}
